295f26bec3e7c867e0fbb8f98187733f3997336b
Wendy Rivero <60824265+RiveroWendy@users.noreply.github.com>
2023-02-27 18:21:38 -0300

    Add write & read cuenta_bancarias.bin

--- Finals/Final-diciembre/main.c
+++ Finals/Final-diciembre/main.c
@@ -2,81 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Declaro prototipo de funciones a utilizar
-struct registro
-{
-    int cuenta;
-    float monto;
-    int operacion;
-};
-
 struct binario
 {
     int numero;
     float saldo;
     char nombre[50];
 };
-void imprimirMenu();
-void tomarValorMenu(int *);
 
-void cargarDatos();
-void ingresoManual();
-
-void verificarMonto();
-void verificarCuenta();
-
-void opcionDebito();
-void opcionCredito();
 void crearCuenta(FILE *, struct binario *);
-
-void guardarLog();
-
-void iniciarBaseDeDatos();
-void modificarTitular();
-void consultarCuentas();
-void guardarBaseDeDatos();
+void consultarCuentas(FILE *);
 
 int main()
 {
-    FILE *movimientos, *cuentasBancarias, *log;
-    int contador = 0, opcion = 0, opcionGuardar = 0, opcionGuardada = 0;
-    struct registro dato;
+    FILE *cuentasBancarias;
     struct binario dato_grabar;
-    //imprimirMenu();
+
     crearCuenta(cuentasBancarias, &dato_grabar);
-    consultarCuentas();
+    consultarCuentas(cuentasBancarias);
+
     return 0;
 }
-/*void imprimirMenu()
-{
 
-    printf("\nBIENVENIDO!\n\nSeleccione una opcion: \n");
-
-    printf("1 - Operaciones desde archivo\n");
-
-    printf("2 - Ingreso nueva cuenta\n");
-
-    printf("2 - Procesar transacciones\n");
-
-    printf("3 - Modificar nombre del titular\n");
-
-    printf("4 - Consultar estados de cuenta\n");
-
-    printf("5 - Guardar base de datos\n");
-
-    printf("6 - Salir del programa\n");
-
-}
-*/
-void crearCuenta(FILE *  cuentasBancarias, struct binario *dato_grabar)
+void crearCuenta(FILE *cuentasBancarias, struct binario *dato_grabar)
 {
     cuentasBancarias = fopen("cuentas_bancarias.bin", "ab");
 
     if(!cuentasBancarias)
-       {
-           printf("Error al abrir el archivo\n");
-            return;
-       }
+    {
+        printf("Error al abrir el archivo\n");
+        return;
+    }
 
     printf("\nOpcion Crear Cuenta \n");
 
@@ -90,27 +45,27 @@ void crearCuenta(FILE *  cuentasBancarias, struct binario *dato_grabar)
     fwrite(dato_grabar, sizeof(struct binario), 1, cuentasBancarias);
     fclose(cuentasBancarias);
 }
-void consultarCuentas()
+
+void consultarCuentas(FILE *cuentasBancarias)
 {
-  FILE *binaryFile;
-struct binario dato_leer;
+    cuentasBancarias = fopen("cuentas_bancarias.bin", "rb");
 
-// Open binary file in read mode
-binaryFile = fopen("cuentas_bancarias.bin", "rb");
+    if(!cuentasBancarias)
+    {
+        printf("Error al abrir el archivo\n");
+        return;
+    }
 
-// Check if file was opened successfully
-if (binaryFile == NULL) {
-    printf("Error opening binary file");
-    return;
-}
+    struct binario dato;
 
-// Read data from binary file
-while (fread(&dato_leer, sizeof(struct binario), 1, binaryFile) == 1) {
-    printf("Numero de Cuenta: %d\n", dato_leer.numero);
-    printf("Saldo: %f\n", dato_leer.saldo);
-    printf("Nombre de Titular:%s\n", dato_leer.nombre);
-}
+    rewind(cuentasBancarias);
 
-// Close binary file
-fclose(binaryFile);
+    while(fread(&dato, sizeof(struct binario),1,cuentasBancarias) == 1)
+    {
+        printf("Numero de cuenta: %d\n", dato.numero);
+        printf("Saldo: %.2f\n", dato.saldo);
+        printf("Titular: %s\n\n", dato.nombre);
+    }
+
+    fclose(cuentasBancarias);
 }

4b0249610f95be18a3abe8910fc7f480b59ca528
Wendy Rivero <60824265+RiveroWendy@users.noreply.github.com>
2023-02-28 00:00:59 -0300

    Add BuscarCuenta, VerificarNumero

--- Finals/Final-diciembre/main.c
+++ Finals/Final-diciembre/main.c
@@ -9,14 +9,24 @@ struct binario
     char nombre[50];
 };
 
+struct registro
+{
+    int cuenta;
+    float monto;
+    int operacion;
+};
+
 void crearCuenta(FILE *, struct binario *);
 void consultarCuentas(FILE *);
 
+int verificarNumero(int);
+int buscarCuenta(FILE *, int);
+
 int main()
 {
-    FILE *cuentasBancarias;
+    FILE *cuentasBancarias = NULL, *movimientos = NULL, *log=NULL;
     struct binario dato_grabar;
-
+    int numeroCuenta = 12345678;
     crearCuenta(cuentasBancarias, &dato_grabar);
     consultarCuentas(cuentasBancarias);
 
@@ -26,7 +36,7 @@ int main()
 void crearCuenta(FILE *cuentasBancarias, struct binario *dato_grabar)
 {
     cuentasBancarias = fopen("cuentas_bancarias.bin", "ab");
-
+    int num_digitos = 0;
     if(!cuentasBancarias)
     {
         printf("Error al abrir el archivo\n");
@@ -35,8 +45,17 @@ void crearCuenta(FILE *cuentasBancarias, struct binario *dato_grabar)
 
     printf("\nOpcion Crear Cuenta \n");
 
+   do {
     printf("\n Ingrese Numero de cuenta:\n");
     scanf("%d", &dato_grabar->numero);
+    num_digitos = verificarNumero(dato_grabar->numero);
+    if(buscarCuenta(cuentasBancarias, dato_grabar->numero))
+        {
+            printf("El numero de cuenta ya existe. Intente nuevamente.\n");
+            num_digitos = 0;
+        }
+    } while(num_digitos != 8);
+
     printf("\n Ingrese Saldo:\n");
     scanf("%f", &dato_grabar->saldo);
     printf("\nIngrese numero de titular\n");
@@ -68,4 +87,47 @@ void consultarCuentas(FILE *cuentasBancarias)
     }
 
     fclose(cuentasBancarias);
+}
+
+int verificarNumero(int numero)
+{
+    int tamanio = 0;
+    int temp = numero;
+
+    while (temp != 0)
+    {
+        temp /= 10;
+        tamanio++;
+    }
+
+    return tamanio;
+}
+
+
+int buscarCuenta(FILE * cuentasBancarias, int numeroCuenta)
+{
+cuentasBancarias = fopen("cuentas_bancarias.bin", "rb");
+struct binario dato;
+if (!cuentasBancarias)
+    {
+        printf("Error al abrir el archivo\n");
+        return -1;
+    }
+
+    rewind(cuentasBancarias);
+    while(fread(&dato, sizeof(struct binario), 1 , cuentasBancarias) == 1)
+        {
+            if(dato.numero == numeroCuenta)
+                {
+                    printf("Cuenta Encontrada\n");
+                    printf("Numero de cuenta: %d\n", dato.numero);
+                    printf("Saldo: %.2f\n", dato.saldo);
+                    printf("Titular: %s\n\n", dato.nombre);
+                    fclose(cuentasBancarias);
+                    return 1;
+                }
+        }
+        printf("Cuenta no encontrada\n");
+        fclose(cuentasBancarias);
+        return 0;
 }
